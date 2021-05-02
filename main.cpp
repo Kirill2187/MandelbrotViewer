b2481d6bfa56f508970fc9338124f361fbfecaf8
Kirill Pupkov <pupkovkirill@gmail.com>
2021-05-02 16:07:27 +0500

    Selection Box Added

--- main.cpp
+++ main.cpp
@@ -1,43 +1,74 @@
 #include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 #include "iostream"
 
 using namespace sf;
 
-int width = 800, height = 600;
+unsigned int width = 800, height = 600;
+RenderWindow window;
+
+bool isBoxActive = false;
+Vector2i lastPressPosition;
+
+Color boxColor = Color(255, 50, 50);
+
+void zoom(Vector2i p1, Vector2i p2) {
+
+}
+
+void processEvent(Event &event) {
+    if (event.type == Event::Closed) {
+        window.close();
+    }
+    else if (event.type == Event::Resized) {
+        width = event.size.width;
+        height = event.size.height;
+        sf::FloatRect visibleArea(0, 0, width, height);
+        window.setView(sf::View(visibleArea));
+    }
+    else if (event.type == Event::MouseButtonPressed) {
+        isBoxActive = true;
+        lastPressPosition = {event.mouseButton.x, event.mouseButton.y};
+    }
+    else if (event.type == Event::MouseMoved) {
+
+    }
+    else if (event.type == Event::MouseButtonReleased) {
+        if (isBoxActive)
+            zoom(lastPressPosition, {event.mouseButton.x, event.mouseButton.y});
+        isBoxActive = false;
+    }
+    else if (event.type == sf::Event::LostFocus) {
+        isBoxActive = false;
+    }
+}
+
+void drawBox() {
+    Vector2i mousePos = Mouse::getPosition(window);
+
+    RectangleShape rect(Vector2f((mousePos.x - lastPressPosition.x),
+                                 (mousePos.y - lastPressPosition.y)));
+    rect.setFillColor(Color::Transparent);
+    rect.setPosition(lastPressPosition.x, lastPressPosition.y);
+    rect.setOutlineThickness(1);
+    rect.setOutlineColor(boxColor);
+    window.draw(rect);
+}
 
 int main() {
-
-    Window window(VideoMode(width, height), "Test window");
+    window.create(VideoMode(width, height), "Test window");
 
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
-            if (event.type == Event::Closed) {
-                window.close();
-            }
-            else if (event.type == Event::Resized) {
-                width = event.size.width;
-                height = event.size.height;
-            }
-            else if (event.type == Event::MouseButtonPressed) {
-                std::cout << "Mouse button pressed: ";
-                std::cout << event.mouseButton.x << " " << event.mouseButton.y << std::endl;
-            }
-            else if (event.type == Event::MouseMoved) {
-                std::cout << "Mouse button moved: ";
-                std::cout << event.mouseMove.x << " " << event.mouseMove.y << std::endl;
-                if (Mouse::isButtonPressed(sf::Mouse::Left)) {
-
-                }
-            }
-            else if (event.type == Event::MouseButtonReleased) {
-                std::cout << "Mouse button released: ";
-                std::cout << event.mouseButton.x << " " << event.mouseButton.y << std::endl;
-            }
-            else if (event.type == sf::Event::LostFocus) {
-
-            }
+            processEvent(event);
         }
+        window.clear();
+
+        if (isBoxActive)
+            drawBox();
+
+        window.display();
     }
 
     return 0;
